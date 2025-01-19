628100e31508412f2b2ef3595be364d2d7eb5134
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-01-19 15:03:21 +0100

    connecter MongoDB

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,28 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
+// Chargement des variables d'environnement
 dotenv.config();
+
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("MongoDB connecté"))
+.catch(err => console.error(err));
 
+
+// Route de test
 app.get("/", (req, res) => {
     res.send("Bienvenue sur À mots ouverts !");
 });
 
+// Démarrage du serveur
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Serveur lancé sur http://localhost:${PORT}`));
