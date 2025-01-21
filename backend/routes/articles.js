06e3c325996c39cedd81fbb8089097c7566957c5
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-01-21 03:17:49 +0100

    backend - routes- ajouter les articles -

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -1,11 +1,21 @@
 import express from "express";
 import Article from "../models/Article.js";
+import authMiddleware from "../middlewares/auth.js"; // Protection admin
 
 const router = express.Router();
 
+// Récupérer tous les articles (triés du plus récent au plus ancien)
 router.get("/", async (req, res) => {
     const articles = await Article.find().sort({ date: -1 });
     res.json(articles);
+});
+
+// Ajouter un article (admin uniquement)
+router.post("/", authMiddleware, async (req, res) => {
+    const { title, subtitle, content, image } = req.body;
+    const newArticle = new Article({ title, subtitle, content, image });
+    await newArticle.save();
+    res.json(newArticle);
 });
 
 export default router;
